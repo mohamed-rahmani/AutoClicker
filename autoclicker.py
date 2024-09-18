93f98b402c31a0c0edbcc8660054d887ad192f19
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-09-18 23:23:20 +0200

    Using xdotool for click

--- autoclicker.py
+++ autoclicker.py
@@ -1,23 +1,25 @@
 import time
+import subprocess
 import threading
-from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
 
 TOGGLE_KEY = KeyCode(char="p")
+STOP_KEY = KeyCode(char="o")
 
 clicking = False
-mouse = Controller()
 
 def clicker():
     while True:
         if clicking:
-            mouse.click(Button.left, 1)
+            subprocess.call(['xdotool', 'click', '1'])
         time.sleep(0.001)
 
 def toggle_event(key):
     if key == TOGGLE_KEY:
         global clicking
         clicking = not clicking
+    if key == STOP_KEY:
+        clicking = False
 
 click_thread = threading.Thread(target=clicker)
 click_thread.start()
